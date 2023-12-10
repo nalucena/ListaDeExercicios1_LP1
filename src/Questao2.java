88d7abebcf37f04c641b04039b3029c0af14ce0f
Nathalya Lucena <108847899+nalucena@users.noreply.github.com>
2023-12-10 15:18:03 -0300

    Update Questao2.java
    igual a questão 1

--- src/Questao2.java
+++ src/Questao2.java
@@ -1,2 +1,39 @@
-package PACKAGE_NAME;public class Questao2 {
+import java.util.Scanner;
+
+public class Questao2 {
+
+    /* Escreva um programa que captura nome, salário, idade, altura e
+sexo. Mostre na tela utilizando o printf.*/
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        String nome, sexo;
+        int idade;
+        double salario, altura;
+
+        System.out.println("Digite as informações abaixo solicitadas: ");
+
+        System.out.println("Digite seu nome: ");
+        nome = input.nextLine();
+        System.out.println("Digite o seu salário: ");
+        salario = input.nextDouble();
+        System.out.println("Digite sua idade: ");
+        idade = input.nextInt();
+        System.out.println("Digite sua altura em metros: ");
+        altura = input.nextDouble();
+        System.out.println("Digite o seu sexo: ");
+        sexo = input.next();
+
+        System.out.printf("%nOs dados informados são:%n"
+                + "Nome: %s%n"
+                + "Salário: %.2f%n"
+                + "Idade: %d%n"
+                + "Altura: %.2f%n"
+                + "Sexo: %s%n", nome, salario, idade, altura, sexo);
+
+
+
+    }
 }
