6f518ef8277f34fd62369f19401ecd59f012f643
Nathalya Lucena <108847899+nalucena@users.noreply.github.com>
2023-12-10 15:17:20 -0300

    Update Questao1.java
    Erro no commit, questão aparecendo incompleta

--- src/Questao1.java
+++ src/Questao1.java
@@ -1,2 +1,31 @@
-package PACKAGE_NAME;public class Questao1 {
+import java.util.Scanner;
+
+public class Questao1 {
+
+    /* Escreva um programa que captura nome, idade e profissão do
+    usuário e mostra na tela os valores. (Utilize a classe Scanner e
+    os 3 métodos print, printf e println).*/
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        String nome, profissao;
+        int idade;
+
+        System.out.println("Para iniciarmos, por favor, digite as informações solicitadas: ");
+        System.out.println("Digite seu nome: ");
+        nome = input.nextLine();
+        System.out.println("Digite sua idade: ");
+        idade = input.nextInt();
+        System.out.println("Digite sua profissão: ");
+        profissao = input.next();
+
+        System.out.printf("%nOs dados informados são:%n"
+                + "Nome: %s%n"
+                + "Idade: %d%n"
+                + "Profissão: %s%n", nome, idade, profissao);
+
+
+    }
 }
